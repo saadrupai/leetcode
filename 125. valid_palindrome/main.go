4564488918b829128d7585b771c76b45c19a2f5b
saadrupai <saadrupai.work@gmail.com>
2025-02-04 22:09:12 +0600

    solve 125 is valid palindrome

--- 125. valid_palindrome/main.go
+++ 125. valid_palindrome/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func isPalindrome(s string) bool {
-
+	alphaNum := []rune{}
+	lowerS := strings.ToLower(s)
+	for _, char := range lowerS {
+		if (char >= 'a' && char <= 'z') ||
+			(char >= '0' && char <= '9') {
+			alphaNum = append(alphaNum, char)
+		}
+	}
+	str := string(alphaNum)
+	println(str)
+	for i, j := 0, len(alphaNum)-1; i < j; i, j = i+1, j-1 {
+		alphaNum[i], alphaNum[j] = alphaNum[j], alphaNum[i]
+	}
+	rstr := string(alphaNum)
+	println(rstr)
+	if str == rstr {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
