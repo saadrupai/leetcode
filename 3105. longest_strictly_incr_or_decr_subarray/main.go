f88f3c21f0c2e0dd61a9355512ec014770758b97
saad <saad.rupai@vivasoftltd.com>
2025-02-04 19:23:00 +0600

    make file fort 3105

---
+++ 3105. longest_strictly_incr_or_decr_subarray/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func longestMonotonicSubarray(nums []int) int {
+    
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(longestMonotonicSubarray(nums))
+}
