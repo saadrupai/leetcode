5f98773c7c665d2ebea41473e886bd8af179f677
saadrupai <saadrupai.work@gmail.com>
2025-02-06 01:36:58 +0600

    approach 3 sum

--- 15. 3sum/main.go
+++ 15. 3sum/main.go
@@ -1,12 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func threeSum(nums []int) [][]int {
+	res := [][]int{}
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j, k := i+1, len(nums)-1
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+			println(sum, nums[i], nums[j], nums[k])
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				j++
+				k--
+				for j < k && nums[j] == nums[j-1] {
+					j++
+				}
+				for j < k && nums[k] == nums[k+1] {
+					k--
+				}
+			} else if sum < 0 {
+				j++
+			} else {
+				k--
+			}
+		}
+	}
 
+	return res
 }
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
-
 	fmt.Println(threeSum(nums))
 }
