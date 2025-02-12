b6ce46f74a1f953ccf21eaf129deb0b3e1dad4a7
saadrupai <saadrupai.work@gmail.com>
2025-02-12 23:26:17 +0600

    init solve 338

---
+++ 338. counting_bits/main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+func countBits(n int) []int {
+	resp := []int{}
+	for i := 0; i <= n; i++ {
+		div := i / 2
+		carry := i % 2
+		count := 1
+		if i == 0 {
+			count = 0
+		}
+		for div != 0 {
+			if carry > 0 {
+				count++
+			}
+			carry = div % 2
+			div = div / 2
+		}
+		resp = append(resp, count)
+	}
+	return resp
+}
+
+func main() {
+	ip := 5
+	fmt.Println(countBits(ip))
+}
+
+// Code
+// Testcase
+// Test Result
+// Test Result
+// 338. Counting Bits
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// Example 2:
+
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+// Constraints:
+
+// 0 <= n <= 105
+
+// Follow up:
+
+// It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+// Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
